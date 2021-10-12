3102c4352fe532bc0f1b7ad64c3f39e0bc82ea48
Marian <marianstowerst@gmail.com>
2021-10-13 00:57:33 +0300

    add Facker.Create<T>()

--- Faker/FakerLib/Faker.cs
+++ Faker/FakerLib/Faker.cs
@@ -15,6 +15,8 @@ namespace FakerLib
         private Dictionary<Type, IBasicTypeGenerator> BasicGenerators = new Dictionary<Type, IBasicTypeGenerator>();
         private Dictionary<Type, ICollectionTypeGenerator> CollectionGenerators = new Dictionary<Type, ICollectionTypeGenerator>();
 
+        private Stack<Type> nestedTypes = new Stack<Type>();
+
 
         public Faker()
         {
@@ -54,7 +56,107 @@ namespace FakerLib
 
         public T Create<T>()
         {
-            return (T)new object();
+
+            Type currType = typeof(T);
+            nestedTypes.Push(currType);
+
+            object nestedRes = GetGenerateValueIfExict(currType);
+            if (nestedRes != null)
+            {
+                nestedTypes.Pop();
+                return (T)nestedRes;
+            }
+
+            T result = default(T);
+            try
+            {
+                result = Activator.CreateInstance<T>();
+            }
+            catch(MissingMethodException)
+            {
+                var typeConstructors = currType.GetConstructors();
+                if(typeConstructors.Length == 0)
+                {
+                    nestedTypes.Pop();
+                    return result;
+                }
+                foreach(ConstructorInfo constructor in typeConstructors)
+                {
+                    List<object> values = new List<object>();
+                    foreach(ParameterInfo param in constructor.GetParameters())
+                    {
+                        values.Add(GetGenerateValue(param.ParameterType));
+                    }
+
+                    result = (T)constructor.Invoke(values.ToArray());
+                }
+            }
+            catch(Exception ex)
+            {
+                nestedTypes.Pop();
+                throw ex;
+            }
+
+            if (currType.IsValueType)
+            {
+                nestedTypes.Pop();
+                return result;
+            }
+
+            foreach (PropertyInfo property in currType.GetProperties())
+            {
+                if (property.CanWrite) { property.SetValue(result, GetGenerateValue(property.PropertyType)); }
+            }
+
+            foreach (FieldInfo f in currType.GetFields())
+            {
+                f.SetValue(result, GetGenerateValue(f.FieldType));
+            }
+
+            nestedTypes.Pop();
+            return result;
+        }
+
+        private object GetGenerateValue(Type desType)
+        {
+            object value = default(object);
+            if (BasicGenerators.ContainsKey(desType))
+            {
+                value = BasicGenerators[desType].Generate();
+            }
+            else if (!nestedTypes.Contains(desType))
+            {
+                value = this.GetType().GetMethod("Create").MakeGenericMethod(desType).Invoke(this, null);
+            }
+
+            return value;
+        }
+
+
+        private object GetGenerateValueIfExict(Type desType)
+        {
+            foreach(Type t in BasicGenerators.Keys)
+            {
+                if (BasicGenerators[t].ResType == desType) {
+                    return BasicGenerators[t].Generate();
+                }
+            }
+
+            if (!desType.IsGenericType)
+                return null;
+
+            foreach (Type t in CollectionGenerators.Keys)
+            {
+                if (CollectionGenerators[t].CollectionTypes.Contains(desType.GetGenericTypeDefinition()))
+                {
+                    var type = CollectionGenerators[t].GetType();
+                    var method = type.GetMethod("Generate");
+                    var memberType = desType.GenericTypeArguments.First();
+                    var res = (object)method.MakeGenericMethod(memberType).Invoke(CollectionGenerators[t], new object[] { this });
+                    return res;
+                }
+            }
+            return null;
         }
     }
 }
